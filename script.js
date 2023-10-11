fa68f0e2aa131f99a6bb02fabc8ba01b998e8573
Min Khant Kyaw <mkhantkyaw61@gmail.com>
2023-10-11 01:19:25 +0100

    subjects added

---
+++ script.js
@@ -0,0 +1,41 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        sitename: "After School Club",
+        lessons: [{
+            subject: "Math",
+            location: "London",
+            price: 100,
+            spaces: 5,
+            image: "images/math.png"
+        },
+        {
+            subject: "Math",
+            location: "London",
+            price: 100,
+            spaces: 5,
+            image: "images/math.png"
+        },
+        {
+            subject: "Math",
+            location: "Yangon",
+            price: 100,
+            spaces: 5,
+            image: "images/math.png"
+        },
+        {
+            subject: "Math",
+            location: "London",
+            price: 100,
+            spaces: 5,
+            image: "images/math.png"
+        },
+        {
+            subject: "Math",
+            location: "London",
+            price: 100,
+            image: "images/math.png"
+        },
+        ]
+    }
+});
