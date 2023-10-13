a5127e2f4814ca42d3edd37b524f32a3a2b31d34
Min Khant Kyaw <mkhantkyaw61@gmail.com>
2023-10-13 17:22:36 +0100

    cart functionality completed

--- script.js
+++ script.js
@@ -2,63 +2,77 @@ const app = new Vue({
     el: '#app',
     methods: {
         addClub: function (id) {
-            this.cart.push(id);
-            this.count++;
-            this.lessons.filter(function (lesson) {
-                if (lesson['id'] == id)
-                    return (lesson['stock']--);
-            })[0];
-            console.log(this.lessons)
+            const cartItem = this.carts.find((cart) => cart.id === id)
+            if (cartItem) {
+                cartItem.count++
+            }
+            else {
+                this.carts.push({ id: id, count: 1 })
+            }
+            const selectedLesson = this.lessons.find((lesson) => lesson.id === id)
+            selectedLesson.spaces--
+        },
+        canAddToCart: function (id) {
+            const selectedLesson = this.lessons.find((lesson) => lesson.id === id)
+            if (selectedLesson) {
+                return selectedLesson.spaces > 0
+            } else return false
         }
+    },
+    computed: {
+        cartItemCount: function () {
+            if (!this.carts) return 0
+            let count = 0
+            this.carts.forEach(cart => {
+                count += cart.count
+            })
+            return count
+        },
+
+
     },
     data: {
         sitename: "After School Club",
-        count: 0,
         lessons: [{
+            id: 0,
+            subject: "Math",
+            location: "London",
+            price: 100,
+            spaces: 5,
+            image: "images/math.png",
+        },
+        {
             id: 1,
             subject: "Math",
             location: "London",
             price: 100,
             spaces: 5,
             image: "images/math.png",
-            stock: 5,
         },
         {
             id: 2,
+            subject: "Math",
+            location: "Yangon",
+            price: 100,
+            spaces: 5,
+            image: "images/math.png",        },
+        {
+            id: 3,
             subject: "Math",
             location: "London",
             price: 100,
             spaces: 5,
             image: "images/math.png",
-            stock: 5,
-        },
-        {
-            id: 3,
-            subject: "Math",
-            location: "Yangon",
-            price: 100,
-            spaces: 5,
-            image: "images/math.png",
-            stock: 5,
         },
         {
             id: 4,
             subject: "Math",
             location: "London",
             price: 100,
+            image: "images/math.png",
             spaces: 5,
-            image: "images/math.png",
-            stock: 5,
-        },
-        {
-            id: 5,
-            subject: "Math",
-            location: "London",
-            price: 100,
-            image: "images/math.png",
-            stock: 5,
         },
         ],
-        cart: []
+        carts: []
     }
 });
