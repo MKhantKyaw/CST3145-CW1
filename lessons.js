f5511b3722b56d5f01eac008acd0226e35469712
Min Khant Kyaw <mkhantkyaw61@gmail.com>
2023-10-20 14:58:42 +0100

    Lessons data added

---
+++ lessons.js
@@ -0,0 +1,182 @@
+const lessons = [
+    {
+        id: 1,
+        subject: "Soccer Club",
+        location: "Manchester",
+        price: 80,
+        spaces: 15,
+        image: "images/soccer-uk.png",
+    },
+    {
+        id: 2,
+        subject: "Science Explorers",
+        location: "Edinburgh",
+        price: 90,
+        spaces: 12,
+        image: "images/science-uk.png",
+    },
+    {
+        id: 3,
+        subject: "Art and Crafts Workshop",
+        location: "Birmingham",
+        price: 70,
+        spaces: 10,
+        image: "images/art-uk.png",
+    },
+    {
+        id: 4,
+        subject: "Drama Society",
+        location: "London",
+        price: 100,
+        spaces: 10,
+        image: "images/drama-uk.png",
+    },
+    {
+        id: 5,
+        subject: "Music Club",
+        location: "Glasgow, UK",
+        price: 85,
+        spaces: 15,
+        image: "images/music-uk.png",
+    },
+    {
+        id: 6,
+        subject: "Debate Team",
+        location: "Oxford, UK",
+        price: 80,
+        spaces: 12,
+        image: "images/debate-uk.png",
+    },
+    {
+        id: 7,
+        subject: "Gardening Club",
+        location: "Cambridge",
+        price: 65,
+        spaces: 15,
+        image: "images/gardening-uk.png",
+    },
+
+    {
+        id: 8,
+        subject: "Martial Arts Class",
+        location: "Liverpool",
+        price: 95,
+        spaces: 10,
+        image: "images/martialarts-uk.png",
+    },
+
+    {
+        id: 9,
+        subject: "Language Club (French)",
+        location: "Bristol",
+        price: 80,
+        spaces: 12,
+        image: "images/french-uk.png",
+    },
+
+    {
+        id: 10,
+        subject: "Astronomy Society",
+        location: "Cardiff",
+        price: 90,
+        spaces: 10,
+        image: "images/astronomy-uk.png",
+    },
+
+    {
+        id: 11,
+        subject: "Chess Club",
+        location: "Leeds",
+        price: 75,
+        spaces: 12,
+        image: "images/chess-uk.png",
+    },
+
+    {
+        id: 12,
+        subject: "Robotics Team",
+        location: "Newcastle",
+        price: 110,
+        spaces: 8,
+        image: "images/robotics-uk.png",
+    },
+
+    {
+        id: 13,
+        subject: "Dance Class",
+        location: "Sheffield",
+        price: 85,
+        spaces: 15,
+        image: "images/dance-uk.png",
+    },
+
+    {
+        id: 14,
+        subject: "Outdoor Adventure Club",
+        location: "Brighton",
+        price: 70,
+        spaces: 20,
+        image: "images/outdoor-uk.png",
+    },
+
+    {
+        id: 15,
+        subject: "Cooking Class",
+        location: "Nottingham",
+        price: 95,
+        spaces: 10,
+        image: "images/cooking-uk.png",
+    },
+
+    {
+        id: 16,
+        subject: "Yoga Club",
+        location: "Belfast",
+        price: 80,
+        spaces: 15,
+        image: "images/yoga-uk.png",
+    },
+
+    {
+        id: 17,
+        subject: "Theater Group",
+        location: "Leicester",
+        price: 100,
+        spaces: 10,
+        image: "images/theater-uk.png",
+    },
+
+    {
+        id: 18,
+        subject: "Environmental Science Club",
+        location: "York",
+        price: 75,
+        spaces: 10,
+        image: "images/environmental-uk.png",
+    },
+
+    {
+        id: 19,
+        subject: "Basketball Club",
+        location: "Aberdeen",
+        price: 90,
+        spaces: 12,
+        image: "images/basketball-uk.png",
+    },
+
+    {
+        id: 20,
+        subject: "STEM Camp",
+        location: "Southampton",
+        price: 120,
+        spaces: 8,
+        image: "images/stem-uk.png",
+    }
+
+
+
+
+
+
+
+]
